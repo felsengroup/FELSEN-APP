a4b31027d928816160f691c1c912fd985f91c8e3
FELSEN <support@felsengroup.ch>
2025-08-06 17:26:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use('/api/license', require('./routes/license')); // <-- Neue Route hier ein
 app.use('/api/ea', require('./routes/ea')); // <-- Neue Route hier einfügen
 app.use('/api/mt5', require('./routes/mt5')); // <-- Neue Route hier einfügen
 app.use('/api/webhooks', require('./routes/webhook')); // <-- Neue Route hier einfügen
+app.use('/api/mt5_api', require('./routes/mt5_api')); // <-- Neue Route hier einfügen
 
 const PORT = process.env.PORT || 5000;
 
