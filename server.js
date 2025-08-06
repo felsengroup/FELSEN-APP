776913178faedd21e2bf34730044904432726e0d
kayaarda25 <122180744+kayaarda25@users.noreply.github.com>
2025-08-06 14:02:48 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Felsen Backend API ist gestartet'));
+
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
